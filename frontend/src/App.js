00dd8b4da8a4b8d8a6a0e6042aec56464ec4e26b
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2022-07-29 00:51:46 +0200

    update init contract with string storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,154 +1,175 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
-import './App.css'
-import SimpleStorage from './artifacts/contracts/SimpleStorage.sol/SimpleStorage.json'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+
+import "./App.css";
+import ExpressionOfPeace from "./artifacts/contracts/ExpressionOfPeace.sol/ExpressionOfPeace.json";
 
 // NOTE: Make sure to change this to the contract address you deployed
-const simpleStorageAddress = '0xde4De608C284709E8980212C7A48B8bcA5b570A2'
+const expressionOfPeaceAddress = "0x6d584295790d2C9f7F2D4249B6CAebC15b1DA682";
 // ABI so the web3 library knows how to interact with our contract
-const simpleStorageAbi = SimpleStorage.abi
+const expressionOfPeaceABI = ExpressionOfPeace;
 
 // NOTE: checkout the API for ethers.js here: https://docs.ethers.io/v5/api/
 // TIP: Remember to console.log something if you are unsure of what is being returned
 
 const App = () => {
-  const [provider, setProvider] = useState()
-  const [inputValue, setInputValue] = useState('')
-  const [value, setValue] = useState('0')
-  const [blockNumber, setBlockNumber] = useState('0')
-  const [gasPrice, setGasPrice] = useState('0')
-  const [account, setAccount] = useState('')
-  const [balance, setBalance] = useState('')
-  const [connected, setConnected] = useState(false)
+  const [provider, setProvider] = useState();
+  const [inputValue, setInputValue] = useState("");
+  const [value, setValue] = useState("...");
+  const [blockNumber, setBlockNumber] = useState("0");
+  const [gasPrice, setGasPrice] = useState("0");
+  const [account, setAccount] = useState("");
+  const [balance, setBalance] = useState("");
+  const [connected, setConnected] = useState(false);
 
   // Will run once everytime a user connects to the dapp
   useEffect(() => {
     // check if ethereum is provided by something like Metamask
-    if (typeof window.ethereum !== 'undefined') {
-      console.log('ethereum is available')
+    if (typeof window.ethereum !== "undefined") {
+      console.log("ethereum is available");
 
       // get provider injected by metamask
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
 
       // Set some data like block number and gas price provided, you can find more options in the API docs
       const setBlockchainData = async () => {
-        setBlockNumber(await provider.getBlockNumber())
-        let gasPrice = await provider.getGasPrice()
+        setBlockNumber(await provider.getBlockNumber());
+        let gasPrice = await provider.getGasPrice();
         // formats a returned big number as gwei where 1,000,000,000 gwei is 1 ether
         // you can read about more denominations here: https://ethdocs.org/en/latest/ether.html
-        gasPrice = Math.trunc(ethers.utils.formatUnits(gasPrice, 'gwei'))
-        setGasPrice(gasPrice)
-      }
+        gasPrice = Math.trunc(ethers.utils.formatUnits(gasPrice, "gwei"));
+        setGasPrice(gasPrice);
+      };
 
       // Set aquired blockchain data as state to use in our frontend
-      setBlockchainData()
+      setBlockchainData();
 
       // Set provider so we can use it in other functions
-      setProvider(provider)
+      setProvider(provider);
     }
-  }, [])
+  }, []);
 
   // handles setting account and balance
   const accountHandler = async (account) => {
-    setAccount(account)
-    const balance = await provider.getBalance(account)
+    setAccount(account);
+    // const balance = await provider.getBalance(account);
     // notice that we use format ether here, uncomment the following console.log and see what happens if we don't
-    setBalance(ethers.utils.formatEther(balance))
-  }
+    // setBalance(ethers.utils.formatEther(balance));
+  };
 
   // handles connecting account
   const connectHandler = async () => {
     // MetaMask requires requesting permission to connect users accounts
     await provider.send("eth_requestAccounts", []);
-    const accountList = await provider.listAccounts()
-    console.log(accountList)
-    accountHandler(accountList[0])
-    setConnected(true)
-  }
+    const accountList = await provider.listAccounts();
+    console.log(accountList);
+    accountHandler(accountList[0]);
+    setConnected(!connected);
+  };
 
   // handles submit button
   const handleSubmit = async (e) => {
     // stops page from refreshing
-    e.preventDefault()
+    e.preventDefault();
 
     // create instance of contract using our contract address, abi, and provider
     const contract = new ethers.Contract(
-      simpleStorageAddress,
-      simpleStorageAbi,
+      expressionOfPeaceAddress,
+      expressionOfPeaceABI,
       provider
-    )
+    );
 
     // a signer is necessary when your want to write to the blockchain
     // your wallet doesn't need to sign or spend any ether to read from the blockchain
     // but it does need to spend ether and therefore sign to write to the blockchain
-    const signer = provider.getSigner()
-    const contractWithSigner = contract.connect(signer)
+    const signer = provider.getSigner();
+    const contractWithSigner = contract.connect(signer);
     // we can use 'set' here because the abi provides us with a reference to the methods defined in our smart contract
-    console.log(await contractWithSigner.set(inputValue))
-    console.log(inputValue)
-  }
+    console.log(await contractWithSigner.set(inputValue));
+    // console.log(inputValue);
+  };
 
   const handleRetrieveData = async () => {
-    const simpleStorageContract = new ethers.Contract(
-      simpleStorageAddress,
-      simpleStorageAbi,
+    const expressionOfPeaceContract = new ethers.Contract(
+      expressionOfPeaceAddress,
+      expressionOfPeaceABI,
       provider
-    )
+    );
     // we can use 'get' here because the abi provides us with a reference to the methods defined in our smart contract
-    setValue(ethers.utils.formatUnits(await simpleStorageContract.get(), 0))
-  }
+
+    //get current value
+    const expressionTxt = await expressionOfPeaceContract.get();
+    // console.log(`Expression of Peace: ${expressionTxt}`);
+    setValue(expressionTxt);
+    // console.log(`value from contract: ${await expressionOfPeaceContract.get()}`);
+  };
 
   return (
-    <div className='layout'>
-      <header className='navbar'>
-        <div className='container'>
-          <div className='logo'>Simple Storage</div>
+    <div className="layout">
+      <header className="navbar">
+        <div className="container">
+          <div className="logo">Expressions of Peace</div>
           {connected ? (
             <div>
-              <label>
+              {/* <label>
                 {`${Number.parseFloat(balance).toPrecision(4)} ETH`}
-              </label>
-              <button className='account-button' onClick={connectHandler}>
-                {account.substring(0, 6)}...
+              </label> */}
+              hi,
+              <button className="account-button" onClick={connectHandler}>
+                {account.substring(0, 5)}...
                 {account.substring(account.length - 4)}
               </button>
             </div>
           ) : (
-            <button className='connect-button' onClick={connectHandler}>
-              Connect
+            <button className="connect-button" onClick={connectHandler}>
+              connect
             </button>
           )}
         </div>
       </header>
-      <section className='cards'>
-        <div className='card'>
-          <h2>Set Value</h2>
-          <form onSubmit={handleSubmit}>
-            <input
-              type='text'
-              required
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-              onKeyPress={(e) => !/[0-9]/.test(e.key) && e.preventDefault()}
-              name='value'
-              placeholder='0'
-            />
-            <button>Submit</button>
-          </form>
-        </div>
-        <div className='card'>
-          <h2>Get Value</h2>
-          <button onClick={handleRetrieveData}>Retrieve</button>
-          <label>{value}</label>
-        </div>
-      </section>
+      {connected ? (
+        <section className="cards">
+          <div className="card">
+            <h2>last expression</h2>
+            <button onClick={handleRetrieveData}>read </button>
+            {/* <p>{value.length > 600 ? (
+              <>
+              {value.substring(0, 400)} ... <a > read all</a>
+              </>
+            ) : value}</p> */}
+             <p>{value}</p>
+          </div>
+          <div className="card">
+            <h2>yours</h2>
+            <form onSubmit={handleSubmit}>
+              <textarea
+                type="text"
+                required
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                // onKeyPress={(e) => e.preventDefault()}
+                // onKeyPress={(e) => !/[0-9]/.test(e.key) && e.preventDefault()}
+
+                name="value"
+                placeholder="how do you imagine?"
+              />
+              <button>sign</button>
+            </form>
+          </div>
+        </section>
+      ) : (
+        <></>
+      )}
+
       <footer>
-        <div className='container'>
-          {gasPrice} gwei &bull; {blockNumber}
+        <div className="container">
+          please do not refresh the page <br></br> until your expression is
+          signed.
+          {/* {gasPrice} gwei &bull; {blockNumber} */}
         </div>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
